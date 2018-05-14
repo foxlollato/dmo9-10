0d9420372b71cdf9c54c25bc33118144add24222
Vitor Hugo Lollato <vitor.lollato@gmail.com>
2018-05-14 11:05:33 -0300

    first commit

--- app/src/main/java/br/edu/ifsp/arq/bancodedados/HotelActivity.java
+++ app/src/main/java/br/edu/ifsp/arq/bancodedados/HotelActivity.java
@@ -1,13 +1,24 @@
 package br.edu.ifsp.arq.bancodedados;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HotelActivity extends AppCompatActivity {
+import br.edu.ifsp.arq.bancodedados.model.Hotel;
+
+public class HotelActivity extends AppCompatActivity implements HotelListFragment.AoClicarNoHotel{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void clicouNoHotel(Hotel hotel) {
+        Intent intent = new Intent(this, HotelDetalheActivity.class);
+        intent.putExtra("hotel", hotel);
+        startActivity(intent);
+    }
+
 }
